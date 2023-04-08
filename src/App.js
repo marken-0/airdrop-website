8ca4fd84d0a9ee61efc2bd2c1d1c39806dee5d7c
Kartikeya Chhikara <kartikeya.chhikara_5221@Kartikeyas-MacBook-Pro.local>
2023-04-09 00:57:48 +0530

    uwu

--- src/App.js
+++ src/App.js
@@ -1,37 +1,45 @@
-import React from 'react';
-import {
-  ChakraProvider,
-  Box,
-  Text,
-  Link,
-  VStack,
-  Code,
-  Grid,
-  theme,
-} from '@chakra-ui/react';
-import { ColorModeSwitcher } from './ColorModeSwitcher';
-import { Logo } from './Logo';
+import React, { useState } from "react";
+import { ChakraProvider, Box, Text, VStack, Grid, theme, Input, Button } from "@chakra-ui/react";
+import { ColorModeSwitcher } from "./ColorModeSwitcher";
+import { Connection, PublicKey, LAMPORTS_PER_SOL, clusterApiUrl } from '@solana/web3.js';
 
 function App() {
+  const [walletAddress, setWalletAddress] = useState('');
+  const [transactionStatus, setTransactionStatus] = useState('');
+
+  const handleAirdrop = async () => {
+    const SOLANA_CONNECTION = new Connection(clusterApiUrl('devnet'));
+    const AIRDROP_AMOUNT = 1 * LAMPORTS_PER_SOL; // 1 SOL 
+
+    try {
+      console.log(`Requesting airdrop for ${walletAddress}`)
+      const signature = await SOLANA_CONNECTION.requestAirdrop(
+          new PublicKey(walletAddress),
+          AIRDROP_AMOUNT
+      );
+      const { blockhash, lastValidBlockHeight } = await SOLANA_CONNECTION.getLatestBlockhash();
+      await SOLANA_CONNECTION.confirmTransaction({
+          blockhash,
+          lastValidBlockHeight,
+          signature
+      },'finalized');
+      setTransactionStatus(`Airdrop Successful: https://explorer.solana.com/tx/${signature}?cluster=devnet`);
+    } catch (error) {
+      console.error(error);
+      setTransactionStatus('Error requesting airdrop, please try again');
+    }
+  }
+
   return (
     <ChakraProvider theme={theme}>
       <Box textAlign="center" fontSize="xl">
         <Grid minH="100vh" p={3}>
           <ColorModeSwitcher justifySelf="flex-end" />
           <VStack spacing={8}>
-            <Logo h="40vmin" pointerEvents="none" />
-            <Text>
-              Edit <Code fontSize="xl">src/App.js</Code> and save to reload.
-            </Text>
-            <Link
-              color="teal.500"
-              href="https://chakra-ui.com"
-              fontSize="2xl"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn Chakra
-            </Link>
+            <Text>Enter your Solana wallet address:</Text>
+            <Input placeholder="Wallet Address" value={walletAddress} onChange={(event) => setWalletAddress(event.target.value)} />
+            <Button onClick={handleAirdrop}>Request Airdrop</Button>
+            {transactionStatus && <Text>{transactionStatus}</Text>}
           </VStack>
         </Grid>
       </Box>
